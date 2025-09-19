cb58393b2ee1d3cf533edda2a34ce928fadb401d
Navya703 <your_email@example.com>
2025-09-19 12:01:53 +0530

    ..

---
+++ day-8/function.js
@@ -0,0 +1,17 @@
+ function isODD(a){
+    if(a%2!=0){
+        return "value is true";
+    }else{
+        return "value is false";
+    }
+}
+console.log(" isODD(3)", isODD(3));
+console.log(" isODD(4)", isODD(4));
+function f(){
+   var a=b=20;
+}
+f();
+console.log(" b", b);
+console.log(" a", a);
+console.log(typeof a);
+console.log(typeof b);
